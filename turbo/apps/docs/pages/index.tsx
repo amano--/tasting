c8593e2c11d791653905f6bb8dd036097c63e790
amano-- <amapetas@gmail.com>
2022-04-13 16:26:49 +0900

    1

--- turbo/apps/docs/pages/index.tsx
+++ turbo/apps/docs/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "ui";
 
 export default function Docs() {
+  console.log("");
   return (
     <div>
       <h1>Docs</h1>
--- turbo/packages/ui/Button.tsx
+++ turbo/packages/ui/Button.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
 export const Button = () => {
+  console.log("");
+
   return <button>Boop</button>;
 };
